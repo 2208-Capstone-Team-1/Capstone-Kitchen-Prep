fab1ed1faa67027aa6835aa77b961c3ce74e7f87
Antongeny <hang.anthony@gmail.com>
2023-01-12 21:05:19 -0500

    initial branch commit, changed console.log for port listening

--- server/index.ts
+++ server/index.ts
@@ -6,7 +6,7 @@ const init = async () => {
   try {
     await seed();
     const port = process.env.PORT || 3000;
-    app.listen(port, () => console.log(`listening on port ${port}`));
+		app.listen(port, () => console.log(`🚀listening on port: ${port} 🔗http://localhost:${port}`));
   } catch (ex) {
     console.log(ex);
   }
