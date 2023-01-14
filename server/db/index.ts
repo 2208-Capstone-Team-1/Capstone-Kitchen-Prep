8a60d3eefc3227434e6fbe4e55cc126aa8d6535f
Yijin Ro <yri.carmine@gmail.com>
2023-01-14 14:38:54 -0500

    added all models and relation between them

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,11 +1,36 @@
 import db from "./db";
 import User from "./User";
 import seed from "./seed";
+import Recipe from "./Recipe";
+import Chatlog from "./Chatlog";
+import Ingredient from "./Ingredient";
 
 const data = {
   User,
   seed,
   db,
+  Recipe,
+  Chatlog,
+  Ingredient,
 };
+
+
+// User & Recipe
+User.hasMany(Recipe);
+Recipe.belongsTo(User);
+
+// User & Chatlog
+User.hasMany(Chatlog);
+Chatlog.belongsTo(User);
+
+// Recipe & Chatlog
+Recipe.hasMany(Chatlog);
+Chatlog.belongsTo(Recipe);
+
+// Ingredients & Others
+User.hasMany(Ingredient);
+Ingredient.belongsTo(User);
+Recipe.hasMany(Ingredient);
+Ingredient.hasMany(Recipe);
 
 export default data;
