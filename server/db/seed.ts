271f54439ce6082df8d4f8ef5589616e8b33fdc5
Yijin Ro <yri.carmine@gmail.com>
2023-01-14 14:39:38 -0500

    cleaned up

--- server/db/seed.ts
+++ server/db/seed.ts
@@ -4,7 +4,7 @@ import User from "./User";
 const seed = async () => {
   await db.sync({ force: true });
 
-  const [moe, lucy, larry, ethyl] = await Promise.all([
+  const [moe, lucy, larry, ethyl, admin] = await Promise.all([
     User.create({ first_name: "moe", last_name: "johns", password: "123", email: "moe@gmail.com", phoneNumber:"2013456789", isAdmin: false }),
     User.create({ first_name: "lucy", last_name: "lee", password: "123", email: "lucy@gmail.com", phoneNumber:"2015429874", isAdmin: false  }),
     User.create({ first_name: "larry", last_name: "whiton", password: "123", email: "larry@gmail.com", phoneNumber:"2013621785", isAdmin: false  }),
@@ -18,6 +18,7 @@ const seed = async () => {
       lucy,
       larry,
       ethyl,
+      admin,
     },
   };
 };
