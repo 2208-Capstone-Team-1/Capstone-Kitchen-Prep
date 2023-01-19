a5674bc612296f1068ae4d52bca3f2aed567d8cb
Lena Al <lena.al.developer@gmail.com>
2023-01-18 19:49:21 -0800

    created basic API  end points for user model

--- server/api/userRouter.ts
+++ server/api/userRouter.ts
@@ -3,7 +3,9 @@ const User = db.User;
 import express, { NextFunction, Request, Response } from "express";
 const router = express.Router();
 
-// get all users
+/* get all users
+localhost:3000/api/users
+ */
 router.get("/", async (req: Request, res: Response, next: NextFunction) => {
   try {
     const users = await User.findAll();
@@ -12,4 +14,82 @@ router.get("/", async (req: Request, res: Response, next: NextFunction) => {
     next(error);
   }
 });
+
+/* get  a user by id
+localhost:3000/api/users
+ */
+router.get("/:id", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const id: string = req.params.id;
+    if (id) {
+      const user = await User.findByPk(id);
+      res.send(user);
+    }
+  } catch (error) {
+    return res.sendStatus(501).send(error);
+  }
+});
+
+/* create a new user
+localhost:3000/api/users */
+router.post("/", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { first_name, last_name, password, email, phoneNumber, isAdmin } =
+      req.body;
+    const newUser = await User.create({
+      first_name,
+      last_name,
+      password,
+      email,
+      phoneNumber,
+      isAdmin,
+    });
+    return res.send(204);
+  } catch (error) {
+    return res.sendStatus(501).send(error);
+  }
+});
+
+/* update a new user
+localhost:3000/api/users/:id */
+router.put("/:id", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const id = req.params.id;
+    const { first_name, last_name, password, email, phoneNumber, isAdmin } =
+      req.body;
+    const user = await User.findByPk(id);
+    user?.update({
+      first_name,
+      last_name,
+      password,
+      email,
+      phoneNumber,
+      isAdmin,
+    });
+    res.sendStatus(200);
+  } catch (error) {
+    return res.sendStatus(501).send(error);
+  }
+});
+
+/* delete a new user
+localhost:3000/api/users/:id */
+router.delete(
+  "/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const NOTFOUNDMESSAGE = `The user you are trying to delete does not exist!!`;
+      const id = req.params.id;
+      const userToDelete = await User.findByPk(id);
+      if (!userToDelete) {
+        throw new Error(NOTFOUNDMESSAGE);
+      }
+      await userToDelete.destroy();
+      res.send(202);
+    } catch (error) {
+      return res.sendStatus(404).send(error);
+    }
+  }
+);
+
 export default router;
