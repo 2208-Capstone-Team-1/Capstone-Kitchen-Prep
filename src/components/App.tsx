7804251f02d5fb9edf5d2ee03edbe4d95ba3caf7
Yijin Ro <yri.carmine@gmail.com>
2023-01-20 18:18:34 -0500

    switched nav bar link order

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -38,13 +38,6 @@ const App = () => {
   // if (!user.id) return <Login />;
   return (
     <div>
-      <h1>CHEF'S KISS</h1>
-      <img
-        src="/static/Chef's kiss_logo.jpg"
-        alt="chef's kiss logo"
-        width="100"
-        height="130"
-      ></img>
       <div className="body">
         <div className="main_topbar">
           <p className="main_ptag">place holder</p>
@@ -58,10 +51,10 @@ const App = () => {
         <div>
           <nav className="navbar">
             <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
             <Link to="/user">Account</Link>
             <Link to="/recipe">Recipe of the Day </Link>
             <Link to="/ingredient">Fridge</Link>
-            <Link to="/about">About</Link>
           </nav>
           <Routes>
             <Route path="/" element={<Home />} />
@@ -73,27 +66,6 @@ const App = () => {
             <Route path="/login" element={<Login />} />
           </Routes>
         </div>
-      <div className="main_logoPlace">
-        <h1 className="mainLogoTxt">Chef's Kiss</h1>
-      </div>
-      <div>
-        <nav className="navbar">
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/user">Account</Link>
-          <Link to="/recipe">Recipe of the Day </Link>
-          <Link to="/ingredient">Fridge</Link>
-          
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/user" element={<UserPage />} />
-          <Route path="/recipe" element={<RecipePage />} />
-          <Route path="/ingredient" element={<IngredientPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/savedRecipe" element={<SavedRecipePage />} />
-        </Routes>
-      </div>
     </div>
     </div>
   );
