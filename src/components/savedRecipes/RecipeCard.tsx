ffb1d77f8c0579e1d3ed12acc15de672eaf23bd6
Yijin Ro <yri.carmine@gmail.com>
2023-01-28 13:47:17 -0500

    finally figured out how to evenly space title, note and link!

--- src/components/savedRecipes/RecipeCard.tsx
+++ src/components/savedRecipes/RecipeCard.tsx
@@ -17,11 +17,13 @@ const RecipeCard: React.FC<Props> = ({recipe, index}) => {
     <div className="circle"></div>
       <div className="recipeCard_container">
         <div className="recipeCard_number">{index +1}</div>
-        <div className="recipeCard_name"><p className="recipeCard_ptagOne">{recipe.name}</p></div>
-        <div className="recipeCard_note"><p className="recipeCard_ptagTwo">" {recipe.personal_note} "</p></div>
-        <div className="recipeCard_url">
-          <a className="recipeCard_atag" href={recipe.url}>Click for Recipe</a>
-        </div>
+          <div className="recipeCard_innerbox">
+              <div className="recipeCard_name"><p className="recipeCard_ptagOne">{recipe.name}</p></div>
+              <div className="recipeCard_note"><p className="recipeCard_ptagTwo">"{recipe.personal_note}"</p></div>
+              <div className="recipeCard_url">
+                <a className="recipeCard_atag" href={recipe.url}>Click for full recipe</a>
+              </div>
+          </div>
       </div>
     </div>;
 };
