73b993a4579b02658e6454e68a79e06fbf9e882e
Yijin Ro <yri.carmine@gmail.com>
2023-01-28 12:33:10 -0500

    imported prop from savedRecipe to display data on the front. made several div for aesthetic

--- src/components/savedRecipes/RecipeCard.tsx
+++ src/components/savedRecipes/RecipeCard.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import "./savedRecipe.css"
 
-const RecipeCard = () => {
-  return <div></div>;
+interface recipeType {
+  name: string;
+  personal_note: string;
+  url: string;
+}
+
+
+interface Props{
+  recipe: recipeType;
+  index: number;
+}
+const RecipeCard: React.FC<Props> = ({recipe, index}) => {
+  return <div className="recipeCard_body">
+    <div className="circle"></div>
+      <div className="recipeCard_container">
+        <div className="recipeCard_number">{index +1}</div>
+        <div className="recipeCard_name"><p className="recipeCard_ptagOne">{recipe.name}</p></div>
+        <div className="recipeCard_note"><p className="recipeCard_ptagTwo">{recipe.personal_note}</p></div>
+        <div className="recipeCard_url">
+          <a className="recipeCard_atag" href={recipe.url}>Click for Recipe</a>
+        </div>
+      </div>
+    </div>;
 };
 
 export default RecipeCard;
