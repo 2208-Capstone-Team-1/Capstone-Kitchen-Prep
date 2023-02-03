4fe25c2e0b12e2da02aa2b7be3565417696ac370
Anton Hang <102202852+antongeny@users.noreply.github.com>
2023-02-02 21:07:27 -0500

    Update About.tsx
    change h1 to h2

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,7 @@
 const AboutPage = () => {
   return (
     <div>
-      <h1>About Page</h1>
+      <h2>About Page</h2>
       <p>
         Chef's Kiss is a kitchen helper application that can help you decide
         meal of the day based on ingredients in your fridge!{" "}
