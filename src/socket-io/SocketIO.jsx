750e450b7a2ce1c7a36a640770aff30becfcadf3
Ed McCormick <edward.mccormick3@gmail.com>
2023-02-08 19:57:35 -0600

    we did some stuff

---
+++ src/socket-io/SocketIO.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+
+//! can add this to redux store
+//! to activate; Socket on
+const socket = io();
+
+const SocketIO = () => {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [lastPong, setLastPong] = useState(null);
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      setIsConnected(true);
+    });
+
+    socket.on("disconnect", () => {
+      setIsConnected(false);
+    });
+
+    socket.on("pong", () => {
+      setLastPong(new Date().toISOString());
+    });
+
+    return () => {
+      socket.off("connect");
+      socket.off("disconnect");
+      socket.off("pong");
+    };
+  }, []);
+
+  const sendPing = () => {
+    socket.emit("ping");
+  };
+
+  return (
+    <div>
+      <p>Connected: {"" + isConnected}</p>
+      <p>Last pong: {lastPong || "-"}</p>
+      <button onClick={sendPing}>Send ping</button>
+    </div>
+  );
+};
+
+export default SocketIO;
