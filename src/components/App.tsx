4a1a3a11893ca3b2e37f98ee3a2c91823e3dc53a
Antongeny <hang.anthony@gmail.com>
2023-02-14 17:07:50 -0500

    added links to respective githubs to group member's names

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -97,9 +97,6 @@ const App = () => {
           </nav>
           <RoutesComponent />
         </div>
-      </div>
-      <div>
-        {/* Insert Footer here */}
         <Footer />
       </div>
     </div>
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,5 @@
-
+import GitHubIcon from '@mui/icons-material/GitHub';
+import { Link } from '@mui/material';
 
 const Footer = () => {
 
@@ -6,8 +7,38 @@ const Footer = () => {
     <footer>
       <div className="footer">
         <ul>
-          <li>Brought to you by: Anton, Ed, Carmin, Lena, and Lily</li>
+          <li>Brought to you by: </li>
+
+          <Link href="https://github.com/antongeny" target="_blank">
+            <li>
+              Anton
+            </li>
+          </Link>
+          <Link href="https://github.com/emccormick16" target="_blank">
+            <li>
+              Ed
+            </li>
+          </Link>
+          <Link href="https://github.com/marchingkoala" target="_blank">
+            <li>
+              Carmin
+            </li>
+          </Link>
+          <Link href="https://github.com/lena-A-Al" target="_blank">
+            <li>
+              Lena
+            </li>
+          </Link>
+          <Link href="https://github.com/linglin1638078" target="_blank">
+            <li>
+              Lily
+            </li>
+          </Link>
+          <Link href="https://github.com/2208-Capstone-Team-1/Capstone-Kitchen-Prep" target="_blank">
+            <GitHubIcon/>Our Main Repo
+          </Link>
         </ul>
+        <ul>*Please click on a team member to see their github profile </ul>
       </div>
     </footer>
   );
