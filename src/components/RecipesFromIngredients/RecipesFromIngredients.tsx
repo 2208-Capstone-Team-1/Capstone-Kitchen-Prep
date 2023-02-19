f44f1b60faa2fe31e0ad7d083c160c5037c2e948
Lena Al <lena.al.developer@gmail.com>
2023-02-18 21:32:03 -0800

    update styling for recipe page

--- src/components/RecipesFromIngredients/RecipesFromIngredients.tsx
+++ src/components/RecipesFromIngredients/RecipesFromIngredients.tsx
@@ -6,6 +6,7 @@ import MuiLoader from "../MuiLoader";
 import "../Recipe/recipe.css";
 import { setIngredients } from "../../store/ingredientSlice";
 import "./recipesFromIngredients.css";
+import { Button } from "@mui/material";
 
 // interface recipesObj {
 //   [key: string]: any;
@@ -71,6 +72,12 @@ const RecipesFromIngredients = () => {
 
   const imageURL = recievedRecipesInfo && `${recievedRecipesInfo.image}`;
 
+  // removing the html tags from summary.
+  const regex = /(<([^>]+)>)/gi;
+  const summary =
+    recievedRecipesInfo.summary &&
+    recievedRecipesInfo.summary.replace(regex, "");
+
   return (
     // if loading is false, display an error message
     !loading ? (
@@ -79,80 +86,53 @@ const RecipesFromIngredients = () => {
       </div>
     ) : (
       <>
-        <div className="card-container">
-          <div className="card u-clearfix">
-            <div className="card-body">
-              <span className="card-number card-circle subtle">01</span>
-              <span className="card-author subtle">John Smith</span>
-              <h2 className="card-title">New Brunch Recipe</h2>
-              <span className="card-description subtle">
-                These last few weeks I have been working hard on a new brunch
-                recipe for you all.
-              </span>
-              <div className="card-read">Read</div>
-              <span className="card-tag card-circle subtle">C</span>
-            </div>
-            <img
-              src="https://s15.postimg.cc/temvv7u4r/recipe.jpg"
-              alt=""
-              className="card-media"
-            />
-          </div>
-          <div className="card-shadow"></div>
-        </div>
-        {/* <div classNameNameName="recipe_body">
-          <div classNameNameName="recipe_container">
-            <div classNameNameName="recipe_box">
-              <h1 classNameNameName="recipe_h1">Recipe based on What You Have</h1>
-              <h2 classNameNameName="recipe_h2">{recievedRecipesInfo.title}</h2>
-              <img classNameNameName="recipe_img" src={imageURL} />
-              <h2 classNameNameName="recipe_h2">Ingredients</h2>
-              <div classNameNameName="recipe_ingredients">
-                {recievedRecipesInfo.extendedIngredients?.map(
-                  (foodItem: any, index: number) => {
-                    return (
-                      <div key={index}>
-                        <p classNameNameName="recipe_ptag">
-                          {index + 1}: {foodItem.original}
-                        </p>
-                      </div>
-                    );
-                  }
-                )}
-              </div>
-              <h2 classNameNameName="recipe_h2">Recipe</h2>
-              <div classNameNameName="recipe_instruction">
-                {recievedRecipesInfo.analyzedInstructions[0].steps!.map(
-                  (step: any) => {
-                    return (
-                      <p classNameNameName="recipe_ptag" key={step.number}>
-                        {step.number}: {step.step}
-                      </p>
-                    );
-                  }
-                )}
-              </div>
-              <div classNameNameName="recipe_diet">
-                {recievedRecipesInfo.diets.map(
-                  (diet: string, index: number) => {
-                    return (
-                      <p classNameNameName="recipe_ptag" key={index}>
-                        {" "}
-                        # {diet}
-                      </p>
-                    );
-                  }
-                )}
-              </div>
-              <div classNameNameName="recipe_url">
-                Source:{" "}
-                <a classNameNameName="recipe_atag" href={recievedRecipesInfo.sourceUrl}>
-                  {recievedRecipesInfo.sourceUrl}
-                </a>
+        {
+          <div className="card-container">
+            <div className="card u-clearfix">
+              <div className="card-body">
+                <div className="card-media">
+                  <div>
+                    {" "}
+                    <img src={imageURL} width="350px;" alt="" />
+                  </div>{" "}
+                  <div className="vegan-image">
+                    {recievedRecipesInfo.vegan ? (
+                      <img src="../static/vegan.png" width="60px" alt="" />
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                  <div className="gluten-image">
+                    {recievedRecipesInfo.glutenFree ? (
+                      <img
+                        src="../static/gluten-free.png"
+                        width="60px"
+                        alt=""
+                      />
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                </div>
+                <h2 className="card-title">{recievedRecipesInfo.title}</h2>
+                <span className="card-description subtle">{summary}</span>
+                <div className="card-read">
+                  <a
+                    className="sourceUrl"
+                    href={recievedRecipesInfo.spoonacularSourceUrl}
+                    target="_blank"
+                  >
+                    Read Full Recipe
+                  </a>
+                </div>
+                <div className="saveBtn">
+                  <Button className="textBtn">Save</Button>
+                </div>
               </div>
             </div>
+            <div className="card-shadow"></div>
           </div>
-        </div> */}
+        }
       </>
     )
   );
