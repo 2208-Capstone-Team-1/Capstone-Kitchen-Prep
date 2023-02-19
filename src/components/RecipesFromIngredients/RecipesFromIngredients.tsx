0a47f824fcee395834b26a80396a817035fb2c4a
Lena Al <lena.al.developer@gmail.com>
2023-02-18 19:36:33 -0800

    styling css

--- src/components/RecipesFromIngredients/RecipesFromIngredients.tsx
+++ src/components/RecipesFromIngredients/RecipesFromIngredients.tsx
@@ -5,6 +5,7 @@ import { RootState } from "../../store";
 import MuiLoader from "../MuiLoader";
 import "../Recipe/recipe.css";
 import { setIngredients } from "../../store/ingredientSlice";
+import "./recipesFromIngredients.css";
 
 // interface recipesObj {
 //   [key: string]: any;
@@ -38,6 +39,7 @@ const RecipesFromIngredients = () => {
         if (data[0]?.id) {
           // this is waiting on the new state, not the current state
           const recipeId = data[0].id;
+          console.log("id", recipeId);
           const recipeInfo = await axios.get(
             `https://api.spoonacular.com/recipes/${recipeId}/information?includeNutrition=false&apiKey=bd758414abcc4276ab40dd407756e3d9`
           );
@@ -77,19 +79,40 @@ const RecipesFromIngredients = () => {
       </div>
     ) : (
       <>
-        <div className="recipe_body">
-          <div className="recipe_container">
-            <div className="recipe_box">
-              <h1 className="recipe_h1">Recipe based on What You Have</h1>
-              <h2 className="recipe_h2">{recievedRecipesInfo.title}</h2>
-              <img className="recipe_img" src={imageURL} />
-              <h2 className="recipe_h2">Ingredients</h2>
-              <div className="recipe_ingredients">
+        <div className="card-container">
+          <div className="card u-clearfix">
+            <div className="card-body">
+              <span className="card-number card-circle subtle">01</span>
+              <span className="card-author subtle">John Smith</span>
+              <h2 className="card-title">New Brunch Recipe</h2>
+              <span className="card-description subtle">
+                These last few weeks I have been working hard on a new brunch
+                recipe for you all.
+              </span>
+              <div className="card-read">Read</div>
+              <span className="card-tag card-circle subtle">C</span>
+            </div>
+            <img
+              src="https://s15.postimg.cc/temvv7u4r/recipe.jpg"
+              alt=""
+              className="card-media"
+            />
+          </div>
+          <div className="card-shadow"></div>
+        </div>
+        {/* <div classNameNameName="recipe_body">
+          <div classNameNameName="recipe_container">
+            <div classNameNameName="recipe_box">
+              <h1 classNameNameName="recipe_h1">Recipe based on What You Have</h1>
+              <h2 classNameNameName="recipe_h2">{recievedRecipesInfo.title}</h2>
+              <img classNameNameName="recipe_img" src={imageURL} />
+              <h2 classNameNameName="recipe_h2">Ingredients</h2>
+              <div classNameNameName="recipe_ingredients">
                 {recievedRecipesInfo.extendedIngredients?.map(
                   (foodItem: any, index: number) => {
                     return (
                       <div key={index}>
-                        <p className="recipe_ptag">
+                        <p classNameNameName="recipe_ptag">
                           {index + 1}: {foodItem.original}
                         </p>
                       </div>
@@ -97,23 +120,23 @@ const RecipesFromIngredients = () => {
                   }
                 )}
               </div>
-              <h2 className="recipe_h2">Recipe</h2>
-              <div className="recipe_instruction">
+              <h2 classNameNameName="recipe_h2">Recipe</h2>
+              <div classNameNameName="recipe_instruction">
                 {recievedRecipesInfo.analyzedInstructions[0].steps!.map(
                   (step: any) => {
                     return (
-                      <p className="recipe_ptag" key={step.number}>
+                      <p classNameNameName="recipe_ptag" key={step.number}>
                         {step.number}: {step.step}
                       </p>
                     );
                   }
                 )}
               </div>
-              <div className="recipe_diet">
+              <div classNameNameName="recipe_diet">
                 {recievedRecipesInfo.diets.map(
                   (diet: string, index: number) => {
                     return (
-                      <p className="recipe_ptag" key={index}>
+                      <p classNameNameName="recipe_ptag" key={index}>
                         {" "}
                         # {diet}
                       </p>
@@ -121,15 +144,15 @@ const RecipesFromIngredients = () => {
                   }
                 )}
               </div>
-              <div className="recipe_url">
+              <div classNameNameName="recipe_url">
                 Source:{" "}
-                <a className="recipe_atag" href={recievedRecipesInfo.sourceUrl}>
+                <a classNameNameName="recipe_atag" href={recievedRecipesInfo.sourceUrl}>
                   {recievedRecipesInfo.sourceUrl}
                 </a>
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
       </>
     )
   );
