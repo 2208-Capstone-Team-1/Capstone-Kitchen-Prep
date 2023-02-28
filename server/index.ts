5f0045fd8ddadb4709fff937588c8eb78fb2e595
Antongeny <hang.anthony@gmail.com>
2023-02-27 21:36:52 -0500

    changed seeding scripts around.

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,7 @@
 import app from "./app";
-import seed from "./db/seed";
 
 const init = async () => {
   try {
-    if (process.env.SEED === "true") seed();
     const port = process.env.PORT || 3000;
     app.listen(port, () =>
       console.log(`🚀listening on port: ${port} 🔗http://localhost:${port}`)
